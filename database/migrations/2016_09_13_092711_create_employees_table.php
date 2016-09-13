4a33113167673ca4e57ba009b813554de1d00f42
Arthur <nienkoa@gmail.com>
2016-09-13 12:33:55 +0300

    make auth, create employees migration

---
+++ database/migrations/2016_09_13_092711_create_employees_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('position');
+            $table->float('salary');
+            $table->integer('director_id')->unsigned()->index();
+            $table->dateTime('recruitment_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('employees');
+    }
+}
